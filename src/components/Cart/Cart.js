50593dc5694ab6aa9326c0875244319fc8631859
sheikhrakib0 <rakibrakib0179@gmail.com>
2021-09-24 06:56:46 +0800

    cart update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,11 +7,17 @@ const {cart} = props;
     for(const product of cart){
         total = total + product.price;
     }
+    const shipping = 15;
+    const tax = (total + shipping)*10;
+    const grandTotal = total + shipping + tax;
     return (
         <div>
             <h3>Order Summary</h3>
             <h5>Items Ordered {props.cart.length}</h5>
-            <h5>total: {total}</h5>
+            <h5>total: {total.toFixed(2)}</h5>
+            <p>Shipping: {shipping}</p>
+            <p>Tax: {tax.toFixed(2)}</p>
+            <p>Grand Total: {grandTotal.toFixed(2)}</p>
         </div>
     );
 };
